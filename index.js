441244da169182066f4225516d25e0188ec30a5f
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-07-20 15:08:39 +0700

    feat: add POST method route for creating komik

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     })
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.komik.create(data);
+        res.send(komik);
+    } catch (err) {
+        res.status(err);
+    }
+});

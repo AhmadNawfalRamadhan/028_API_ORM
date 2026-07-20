870b5b3631807499e56adef8e1520eb5a543c7af
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2026-07-20 14:55:50 +0700

    revisi 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db = require('./models');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
 })
 
